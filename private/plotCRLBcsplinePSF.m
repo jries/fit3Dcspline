3ce47bbc20de8de6b88eb55d6f803525204214d0
jries <jonas.ries@gmail.com>
2019-01-31 17:33:17 +0100

    fix

--- private/plotCRLBcsplinePSF.m
+++ private/plotCRLBcsplinePSF.m
@@ -1,8 +1,9 @@
 function plotCRLBcsplinePSF(cspline,ax)
-N=5000;bg=50;x=cspline.x0;y=cspline.x0;z=(1:size(cspline.coeff{1},3))';
+rois=13;
+N=5000;bg=50;x=rois/2;y=rois/2;z=(1:size(cspline.coeff{1},3))';
 v1=ones(length(z),1);
 coords=[v1*x , v1*y , v1* N, v1*bg, z];
-crlb=CalSplineCRLB(cspline.coeff{1}, 13, coords);
+crlb=CalSplineCRLB(cspline.coeff{1}, rois, coords);
 px=120;
 xe=sqrt(crlb(:,1))*px;
 ye=sqrt(crlb(:,2))*px;
@@ -16,4 +17,5 @@ legend('x','y','z','location','north')
 title(ax,'localization precision for N=5000, bg=50')
 xlabel(ax,'z (nm)')
 ylabel(ax,'sqrt(CRLB) in nm')
+grid;set(gca, 'YLim', [3 20])
 end
